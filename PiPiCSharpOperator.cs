b0fa14dbe954d22557e8f879f9fa023606fb01f7
CH-Chang <ch.chang.software@gmail.com>
2024-01-06 17:30:09 +0800

    feat: create first class

---
+++ PiPiCSharpOperator.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="PiPiCSharpOperator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace PiPiCSharp
+{
+    /// <summary>
+    /// PDF Operator.
+    /// </summary>
+    public class PiPiCSharpOperator
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PiPiCSharpOperator"/> class.
+        /// </summary>
+        /// <param name="pdfBytes">The pdf binary bytes.</param>
+        public PiPiCSharpOperator(byte[] pdfBytes)
+        {
+        }
+    }
+}
